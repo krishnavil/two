8ee2c42342b0fae6fea5b42761b7b85fe428e44b
krishnavil <vilaspuremadhukar@gmail.com>
2024-12-17 23:21:57 +0530

    new branch

---
+++ src/main/java/com/project/pages/Signup.java
@@ -0,0 +1,15 @@
+package com.project.pages;
+
+public class Signup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("one");
+		System.out.println("two");
+		System.out.println("three");
+		System.out.println("four");
+
+	}
+
+}
