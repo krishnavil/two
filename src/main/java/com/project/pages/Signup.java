0513ec4f841eae07731312f3820c6bd770a5b6d5
krishnavil <vilaspuremadhukar@gmail.com>
2024-12-18 00:34:12 +0530

    again

--- src/main/java/com/project/pages/Signup.java
+++ src/main/java/com/project/pages/Signup.java
@@ -1,7 +1,11 @@
 package com.project.pages;
 
 public class Signup {
-
+	
+	public void m1()
+	{
+		System.out.println("  ");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
